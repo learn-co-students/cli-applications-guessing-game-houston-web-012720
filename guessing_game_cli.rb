428dde445294235d285d2dbb2e45829020dda83d
anam soomro <anamsoomroed@gmail.com>
2020-01-20 07:47:45 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,11 @@
 def run_guessing_game()
+  #this is a controlled flow
   #number = 0
   #while number == 0 do
   #  number = rand(7)
-  #  rand(max) picks a number between 0 and 6-1 
+  #  rand(max) picks a number between 0 and 7-1 
   #end
+  #just quick maths yo
   number = rand(6) + 1
   #puts "number is #{number}"
   #puts "Guess number 1 through 6?"
